a4750bcf77dff214e4a28c8f3929ff537026f407
harshitchaudhary <harshitchaudhary568@gmail.com>
2025-06-04 10:58:44 +0530

    adding new webpages

---
+++ script.js
@@ -0,0 +1,6 @@
+function see_more() {
+    var see = document.querySelector(".see");
+    see.innerText = "";
+    var seeMore = document.querySelector(".seeMore");
+    seeMore.innerText = "I am passionate about software development and constantly expanding my skills in Java, Javascript, and frontend technologies. With a strong foundation in algorithms, data structure and problem solving, I actively engage in Coding challenges and interactive programming projects.";
+}
