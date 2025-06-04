7d95d237fc441f7a4588540377145d0fd0f3b53d
harshitchaudhary <harshitchaudhary568@gmail.com>
2025-06-04 12:23:27 +0530

    Adding about em information in index page

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function see_more() {
     var seeMore = document.querySelector(".seeMore");
     seeMore.innerText = "I am passionate about software development and constantly expanding my skills in Java, Javascript, and frontend technologies. With a strong foundation in algorithms, data structure and problem solving, I actively engage in Coding challenges and interactive programming projects.";
 }
+
+function seeMore() {
+    var see = document.querySelector("#see");
+    see.innerText = "";
+    var s = document.querySelector("#seeMore").innerText = "I am also preparing for software engineering internships, refining your resume, tackling coding assessments, and exploring opportunities like the Frontend Engineer Intern role. Recently, i've been troubleshooting Java errors, implementing sorting algorithms, and working on interactive projects like Tic-Tac-Toe logic. Your curiosity extends to software testing methodologies, efficient traversal techniques, and compiler optimization concepts. I have thrive on structured learning, detailed explanations, and practical applications, making significant strides toward your goal of becoming a skilled full-stack developer. Keep pushing forward—you're building an impressive technical foundation!";
+}
