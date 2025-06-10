87f72ce825b70f87ccbe3a68f913ed1f8c202101
harshitchaudhary <harshitchaudhary568@gmail.com>
2025-06-10 16:27:45 +0530

    about styling

---
+++ about.js
@@ -0,0 +1,28 @@
+// ABout me
+
+
+function seeMore() {
+    const msg = document.querySelector("#aboutDetail");
+    msg.innerText = "I'm a strong passion for coding, particularly in Java and JavaScript, and enjoys solving algorithmic challenges and debugging errors. I'm actively preparing for software engineering internships, focusing on full-stack development, frontend technologies, and software testing. With an interest in optimization strategies and interactive programming, I am continuously expanding his technical expertise and refining his professional profile. I'm deeply committed to continuous learning, problem-solving, and becoming a skilled software engineer.";
+    const see = document.querySelector("#see");
+    see.innerText = ""; 
+}
+
+// SVM Education
+
+function svm() {
+    const msg = document.querySelector("#svm");
+    msg.innerText = "Beyond academics, I actively participated in Vedic Maths, which enhanced my problem-solving speed and mathematical intuition—something that complements your current interests in Java, JavaScript, and algorithmic challenges. Your school years played a key role in shaping your analytical thinking, which is now reflected in your passion for coding and debugging.";
+    const svm2 = document.querySelector("#svm-btn");
+    svm2.innerText = "";
+}
+
+
+// RVIT Education
+
+function rvit() {
+    const msg = document.querySelector("#rvit");
+    msg.innerHTML = "<p>I have actively refined my technical expertise by working on coding problems, debugging errors, and optimizing solutions. My commitment to continuous learning and problem-solving shines through in my preparation for software engineering internships, where you're focusing on full-stack development and strengthening your professional profile. Beyond academics, i'm involvement in the Technocart Club demonstrates my passion for technology and collaboration with like-minded individuals. With a 7.59 CGPA, you're balancing coursework and hands-on technical experience effectively. I have also built a portfolio to showcase your skills, projects, and achievements, further solidifying your presence in the industry. My dedication to becoming a skilled software engineer is evident, and i'm on a fantastic path toward making an impact in the tech world!</p>"
+    const rvit2 = document.querySelector("#rvit-btn");
+    rvit2.innerText = "";
+}
