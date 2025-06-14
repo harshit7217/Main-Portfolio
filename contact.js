3e2ced9a46652ae23498890695a726679104926a
harshitchaudhary <harshitchaudhary568@gmail.com>
2025-06-14 19:04:06 +0530

    upd

--- contact.js
+++ contact.js
@@ -1,11 +1,11 @@
 document.getElementById("sendButton").addEventListener("click", function () {
     var params = {
-        from_name: document.getElementById("name").value,
-        from_email: document.getElementById("email").value,
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
         message: document.getElementById("message").value
     };
 
-    emailjs.send("service_ryss2zi", "service_ryss2zi", params, "A4zKrFMMPbR7ug_ns", { mode: "cors" } )
+    emailjs.send("service_ryss2zi", "service_ryss2zi", params,) 
         .then(function (response) {
             alert("Email sent successfully!");
         }, function (error) {
